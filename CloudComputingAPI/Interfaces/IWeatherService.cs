67b9fdbf6ed375b2cde12f0f2b1f4813342a6b23
Emre <emre.rauhofer@gmail.com>
2025-11-18 17:08:40 +0100

    Added weather service and interface

---
+++ CloudComputingAPI/Interfaces/IWeatherService.cs
@@ -0,0 +1,15 @@
+﻿using DataAccess.Entities;
+
+namespace CloudComputingAPI.Interfaces
+{
+    public interface IWeatherService
+    {
+        public Task<List<Region>> GetAllRegionsAsync();
+
+        public Task<List<City>> GetCitiesByRegionAsync(int regionId);
+
+        public Task<WeatherData> GetLatestWeatherDataAsync(int cityId);
+
+        public Task<List<WeatherData>> GetAllWeatherDataAsync(int cityId);
+    }
+}
---
+++ CloudComputingAPI/Services/WeatherService.cs
@@ -0,0 +1,58 @@
+﻿using CloudComputingAPI.Exceptions;
+using CloudComputingAPI.Interfaces;
+using DataAccess.Database;
+using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CloudComputingAPI.Services
+{
+    public class WeatherService : IWeatherService
+    {
+        private readonly WeatherDbContext dbContext;
+
+        public WeatherService(WeatherDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public Task<List<Region>> GetAllRegionsAsync()
+        {
+            return Task.FromResult(this.dbContext.Regions.ToList());
+        }
+
+        public Task<List<WeatherData>> GetAllWeatherDataAsync(int cityId)
+        {
+            var city = this.dbContext.Cities.FirstOrDefault(x => x.Id == cityId) ?? throw new NotFoundException("City not found!");
+            var weatherData = this.dbContext.WeatherDatas
+                .Include(x => x.City)
+                .Include(x => x.Condition)
+                .Where(wd => wd.City.Id == cityId)
+                .OrderByDescending(wd => wd.Date)
+                .ToList();
+
+            return Task.FromResult(weatherData);
+        }
+
+        public Task<List<City>> GetCitiesByRegionAsync(int regionId)
+        {
+            var region = this.dbContext.Regions.FirstOrDefault(x => x.Id == regionId) ?? throw new NotFoundException("Region not found!");
+            var cities = this.dbContext.Cities.Include(x => x.Region).Where(c => c.Region.Id == regionId);
+
+            return Task.FromResult(cities.ToList());
+        }
+
+        public Task<WeatherData> GetLatestWeatherDataAsync(int cityId)
+        {
+            var city = this.dbContext.Cities.FirstOrDefault(x => x.Id == cityId) ?? throw new NotFoundException("City not found!");
+            var weatherData = this.dbContext.WeatherDatas
+                .Include(x => x.City)
+                .Include(x => x.Condition)
+                .Where(wd => wd.City.Id == cityId)
+                .OrderByDescending(wd => wd.Date)
+                .FirstOrDefault() ?? throw new NotFoundException("City not found!");
+
+
+            return Task.FromResult(weatherData);
+        }
+    }
+}
